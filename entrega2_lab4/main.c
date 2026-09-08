4e35b148dd9780f95ab6bee37624fffe97872c72
Maira Alejandra <mayisp14@gmail.com>
2026-09-08 14:16:38 -0500

    entrega del laboratorio 2

--- entrega2_lab4/main.c
+++ entrega2_lab4/main.c
@@ -15,14 +15,22 @@ int main(void)
     while(1)
     {
         resultado= variable + variable2;
-        
+        if (resultado>999)
+        {
+            resultado=0;
+            variable=0;
+            variable2=1;
+        }
         uint8_t centenas = resultado / 100;
         uint8_t decenas  = (resultado / 10) % 10;
         uint8_t unidades = resultado % 10;
         variable=variable2;
         variable2=resultado;
-
-        Display_Refresh(centenas, decenas, unidades);
+        for(volatile uint32_t i = 0; i < 100; i++)
+        {
+            Display_Refresh(centenas, decenas, unidades);
+        }
         
+       
     }
 }
